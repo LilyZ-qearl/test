feb08a44e851e35017f9317b2ffa6ae9207c3cab
qearl <1185755742@qq.com>
2019-07-02 13:58:12 +0800

    Create uploadImage.js

---
+++ uploadImage.js
@@ -0,0 +1,55 @@
+const uploadBlobToOssAsync = (Blob, name, dir) => {
+  return new Promise((resolove,reject) => {
+    const ossSignServerAddress = 'https://api-upload.你的域名/xm/oss/web/token?bucket=iamge';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', ossSignServerAddress, false);
+    xhr.send();
+    const signInfo = JSON.parse(xhr.responseText);
+    const fd = new FormData();
+    fd.append('filename', name);
+    fd.append('callback', signInfo.callback);
+    fd.append('expire', signInfo.expire);
+    fd.append('signature', signInfo.signature);
+    fd.append('OSSAccessKeyId',signInfo.accessid);
+    fd.append('success_action_status',status.toString());
+    if(!(dir)) {
+      dir = Blob.type == 'text/html' ? 'html/' : '';
+    }
+    fd.append('key', signInfo.dir + dir + name);
+    fd.append('file', Blob);
+    
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState == 4 && xhr.status == 200) {
+        const result = JSON.parse(xhr.responseText);
+        result.url = result.url.replace(/http:/,'https');
+        resolve(result.url);
+      }
+    }
+    const tempHost = signInfo.host;
+    xhr,open("POST", tempHost);
+    xhr.send(fd);
+  });
+ }
+ 
+ const convertBase64ToBlob = function (base64) {
+    var baseArr = base64.split(',');
+    let imgtype = '';
+    var base64String = '';
+    if(base64Arr.length > 1) {
+      base64String = base64Arr[1];
+      imgtype = base64Arr[0].substring(base65Arr[0].indexOf(':') + 1,base64Arr[0].indexOf(';'));
+    }
+    var bytes = atob(base64String);
+    var bytesCode = new ArrayBuffer(bytes,length);
+    // 转换为类型化数组
+  var byteArray = new Uint8Array(bytesCode);
+
+  // 将base64转换为ascii码
+  for (var i = 0; i < bytes.length; i++) {
+    byteArray[i] = bytes.charCodeAt(i);
+  }
+
+  // 生成Blob对象（文件对象）
+  return new Blob([bytesCode], { type: imgtype });
+ }
+ 

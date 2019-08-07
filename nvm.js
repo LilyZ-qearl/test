601bf6486ed7e32a0a85be09d3ca32596173a06d
qearl <1185755742@qq.com>
2019-08-07 10:20:21 +0800

    Create nvm.js

---
+++ nvm.js
@@ -0,0 +1,11 @@
+1.nvm -v  查看版本号
+2.nvm use <version> eg: nvm use v8.10.0  使用特定Node版本
+3.nvm list 查看所有的node版本
+4.nvm list installed 查看已经安装的版本
+5.nvm list available 查看网络可以安装的版本
+5.nvm ls
+6.nvm current
+7.nvm install 下载
+8.nvm on 打开nodejs控制
+9.nvm off 关闭nodejs控制
+10.nvm proxy 查看设置和代理
